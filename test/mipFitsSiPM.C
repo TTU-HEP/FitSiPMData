99a5c4ed35430e6cac1438d0859b21f619124063
Christopher_Madrid <ChrisMadrid173@gmail.com>
2024-08-17 21:08:38 -0500

    Now looping over all channels

--- test/mipFitsSiPM.C
+++ test/mipFitsSiPM.C
@@ -27,18 +27,18 @@ void fitSPEMIP(TH1* hfit, int run)
     //c1.SetLogx();
     c1.SetLogy();
     //hfit->GetXaxis()->SetRangeUser(10, 400);
-    hfit->GetXaxis()->SetRangeUser(1, 1000);
-    hfit->SetMinimum(0.01);
-    hfit->SetMaximum(1000);
+    hfit->GetXaxis()->SetRangeUser(1, 2000);
+    hfit->SetMinimum(0.1);
+    hfit->SetMaximum(5000);
     
     char ffname[128];
-    double nPeaks = 4;
+    double nPeaks = 6;
     
     PPEFunc background(nPeaks,false);
     background.h = hfit;
     PPEFunc background_MIP(nPeaks,true);
     background_MIP.h = hfit;
-    hfit->Draw("hist");
+    hfit->Draw("E hist");
                                                        //////////////Parameter Info////////////////////////////////
     double min0  =    10; double max0  =     500;      //p[0]  : pedestal amplitude-------------//               //
     double min1  =  10.0; double max1  =    18.0;      //p[1]  : pedestal width-----------------//               //
@@ -77,7 +77,7 @@ void fitSPEMIP(TH1* hfit, int run)
     
     //Fit Pedestal Peak
     sprintf(ffname, "sff%s", hfit->GetName());
-    TF1* fit1 = new TF1(ffname, background, 0.0, 1000.0, 10);
+    TF1* fit1 = new TF1(ffname, background, 20.0, 2000.0, 10);
     fit1->SetParLimits(0,  min0, max0); 
     fit1->SetParLimits(1,  min1, max1); 
     fit1->SetParLimits(2,  min2, max2); 
@@ -113,7 +113,7 @@ void fitSPEMIP(TH1* hfit, int run)
     
     //fit PE Peaks 
     sprintf(ffname, "sff2%s", hfit->GetName());
-    TF1* fit2 = new TF1(ffname, background, 0.0, 1000.0, 10);
+    TF1* fit2 = new TF1(ffname, background, 20.0, 2000.0, 10);
     fit2->SetParLimits(0,  min0, max0); 
     fit2->SetParLimits(1,  min1, max1); 
     fit2->SetParLimits(2,  min2, max2); 
@@ -149,9 +149,9 @@ void fitSPEMIP(TH1* hfit, int run)
 
     //Fine tune PE peaks
     sprintf(ffname, "sff3%s", hfit->GetName());
-    TF1* fit3 = new TF1(ffname, background, 0.0, 1000.0, 10);
-    double down1 = 0.7;
-    double up1   = 1.3;
+    TF1* fit3 = new TF1(ffname, background, 20.0, 2000.0, 10);
+    double down1 = 0.6;
+    double up1   = 1.4;
     fit3->SetParLimits(0,  down1*fit2->GetParameter(0), up1*fit2->GetParameter(0)); 
     fit3->SetParLimits(1,  down1*fit2->GetParameter(1),     fit2->GetParameter(1)); 
     fit3->SetParLimits(2,  down1*fit2->GetParameter(2), up1*fit2->GetParameter(2)); 
@@ -187,7 +187,7 @@ void fitSPEMIP(TH1* hfit, int run)
     
     //Fit MIP peak
     sprintf(ffname, "sff4%s", hfit->GetName());
-    TF1* fit4 = new TF1(ffname, background_MIP, 0.0, 1000.0, 14);
+    TF1* fit4 = new TF1(ffname, background_MIP, 20.0, 2000.0, 14);
     fit4->SetParLimits(0,  min0,  max0); 
     fit4->SetParLimits(1,  min1,  max1); 
     fit4->SetParLimits(2,  min2,  max2); 
@@ -223,9 +223,9 @@ void fitSPEMIP(TH1* hfit, int run)
     
     //Fine Tunning All Fit
     sprintf(ffname, "sff4%s", hfit->GetName());
-    TF1* fit5 = new TF1(ffname, background_MIP, 0.0, 1000.0, 14);
-    double down = 0.7;
-    double up   = 1.3;
+    TF1* fit5 = new TF1(ffname, background_MIP, 20.0, 2000.0, 14);
+    double down = 0.6;
+    double up   = 1.4;
     fit5->SetParLimits(0, down*fit4->GetParameter(0),  up*fit4->GetParameter(0));
     fit5->SetParLimits(1, down*fit4->GetParameter(1),     fit4->GetParameter(1));
     fit5->SetParLimits(2, down*fit4->GetParameter(2),  up*fit4->GetParameter(2));
@@ -267,14 +267,14 @@ void fitSPEMIP(TH1* hfit, int run)
     printf(" P2:%10.4f, P3:%10.4f, P4:%10.4f, P5:%10.4f", fit5->GetParameter(2), fit5->GetParameter(3), fit5->GetParameter(4), fit5->GetParameter(5));
     printf(" P6:%10.4f, P7:%10.4f, P8:%10.4f, P9:%10.4f", fit5->GetParameter(6), fit5->GetParameter(7), fit5->GetParameter(8), fit5->GetParameter(9));
     printf(" P10:%10.4f, P11:%10.4f, P12:%10.4f, P13:%10.4f\n", fit5->GetParameter(10), fit5->GetParameter(11), fit5->GetParameter(12), fit5->GetParameter(13));
-    hfit->GetYaxis()->SetTitle("Events");
+    hfit->GetYaxis()->SetTitle("Events / ADC bin");
     hfit->GetXaxis()->SetTitle("ADC Counts");
     hfit->SetTitleOffset(1,"X");
     hfit->SetTitleOffset(1.2,"Y");
     hfit->SetTitleSize(0.05,"X");
     hfit->SetTitleSize(0.05,"Y");
     
-    TF1* draw5PE = new TF1("BackGround_MIP", background_MIP, 0.0, 1000.0, 14);
+    TF1* draw5PE = new TF1("BackGround_MIP", background_MIP, 20.0, 2000.0, 14);
     draw5PE->FixParameter(0,  fit5->GetParameter(0));
     draw5PE->FixParameter(1,  fit5->GetParameter(1));
     draw5PE->FixParameter(2,  fit5->GetParameter(2));
@@ -293,7 +293,7 @@ void fitSPEMIP(TH1* hfit, int run)
     draw5PE->SetLineColor(kBlue);
     draw5PE->Draw("same");
     
-    TF1* draw5Mip = new TF1("Lang", langaufun, 0.0, 1000.0, 4);
+    TF1* draw5Mip = new TF1("Lang", langaufun, 20.0, 2000.0, 4);
     draw5Mip->FixParameter(0, fit5->GetParameter(12));
     draw5Mip->FixParameter(1, fit5->GetParameter(11)+fit5->GetParameter(2));
     draw5Mip->FixParameter(2, fit5->GetParameter(10));
@@ -306,9 +306,11 @@ void fitSPEMIP(TH1* hfit, int run)
     hfit->SetStats(false);
     hfit->SetTitle("");
     
-    TLatex* CMSPrelim1 = new TLatex(0.14, 0.91, "CMS #scale[0.9]{#font[52]{Preliminary}}");
+    //TLatex* CMSPrelim1 = new TLatex(0.14, 0.91, "CMS #scale[0.9]{#font[52]{Preliminary}}");
+    TLatex* CMSPrelim1 = new TLatex(0.14, 0.91, "Scintillating Fiber");
+    //TLatex* CMSPrelim1 = new TLatex(0.14, 0.91, "Cherenkov Fiber");
     CMSPrelim1->SetNDC();
-    CMSPrelim1->SetTextFont(62);
+    CMSPrelim1->SetTextFont(42);
     
     TLatex* testbeam = new TLatex(0.95, 0.91, "HG-DREAM 2024");
     testbeam->SetNDC();
@@ -357,7 +359,7 @@ void fitSPEMIP(TH1* hfit, int run)
     Gain->SetTextFont(42);
     Gain->SetTextAlign(31);
     
-    //CMSPrelim1->Draw();
+    CMSPrelim1->Draw();
     testbeam->Draw();
     //SiPMTitle->Draw();
     Muon->Draw();
@@ -375,28 +377,70 @@ void fitSPEMIP(TH1* hfit, int run)
 int main()
 {
     char baseFile[]         = "/uscms_data/d3/cmadrid/DREAM/run742_V22000.root";
+    //char baseFile[]         = "/uscms_data/d3/cmadrid/DREAM/run749_4500.root";
     char treeName[]         = "EventTree";
     TChain *chBase = new TChain(treeName);
     chBase->Add(baseFile);
 
     try
     {
-        NTupleReader tr(chBase, {"FERS_Board1_energyHG"});
+        NTupleReader tr(chBase, {"FERS_Board0_energyHG"});
 
-        auto h = std::make_shared<TH1D>("h","h", 1000,0,2000);
-        int chan = 1;
-        
+        std::vector<double> binEdges = {
+            //0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190,
+            0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195,
+            200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390,
+
+            400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 760, 770, 780, 790, 800, 810, 820, 830, 840, 850, 860, 870, 880, 890, 900, 910, 920, 930, 940, 950, 960, 970, 980, 990, 1000, 1010, 1020, 1030, 1040, 1050, 1060, 1070, 1080, 1090, 1100, 1110, 1120, 1130, 1140, 1150, 1160, 1170, 1180, 1190, 1200, 1210, 1220, 1230, 1240, 1250, 1260, 1270, 1280, 1290, 1300, 1310, 1320, 1330, 1340, 1350, 1360, 1370, 1380, 1390, 1400, 1410, 1420, 1430, 1440, 1450, 1460, 1470, 1480, 1490, 1500, 1510, 1520, 1530, 1540, 1550, 1560, 1570, 1580, 1590, 1600, 1610, 1620, 1630, 1640, 1650, 1660, 1670, 1680, 1690, 1700, 1710, 1720, 1730, 1740, 1750, 1760, 1770, 1780, 1790, 1800, 1810, 1820, 1830, 1840, 1850, 1860, 1870, 1880, 1890, 1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000
+
+            //400, 420, 440, 460, 480, 500, 520, 540, 560, 580, 600, 620, 640, 660, 680, 700, 720, 740, 760, 780, 800, 820, 840, 860, 880, 900, 920, 940, 960, 980,
+            //1000, 1025, 1050, 1075, 1100, 1125, 1150, 1175, 1200, 1225, 1250, 1275, 1300, 1325, 1350, 1375, 1400, 1425, 1450, 1475, 1500, 1525, 1550, 1575, 1600, 1625, 1650, 1675, 1700, 1725, 1750, 1775, 1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000
+
+        };
+        std::vector<std::shared_ptr<TH1D>> hVec;
+        bool firstEvent = true;
+
         while(tr.getNextEvent())
         {
             const auto& hg1 = tr.getVec<unsigned short>("FERS_Board1_energyHG");
-            //std::cout<<hg1.size()<<" "<<hg1[5]<<std::endl;
-            if (hg1[chan] < 5000)
+
+            if(firstEvent)
             {
-                h->Fill(hg1[chan]);
+                for(unsigned int i = 0; i < hg1.size(); i++)
+                {
+                    std::string name = "h" + std::to_string(i);
+                    //hVec.push_back(std::make_shared<TH1D>(name.c_str(),name.c_str(), 500,0,2000));
+                    hVec.push_back(std::make_shared<TH1D>(name.c_str(),name.c_str(), binEdges.size()-1, binEdges.data()));
+                }
+                firstEvent = false;
+            };
+
+            for(unsigned int i = 0; i < hg1.size(); i++)
+            {
+                if(hg1[i] > 20 && hg1[i] < 5000)
+                hVec[i]->Fill(hg1[i]);
             }
         }
 
-        fitSPEMIP(h.get(), 1);
+        for(unsigned int i = 0; i < hVec.size(); i++)
+        {
+            if(i == 15 || i == 14 || i == 8)
+            {
+                auto nEvents = hVec[i]->Integral();
+                for(unsigned int j = 0; j < hVec[i]->GetNbinsX()+1; j++)
+                {
+                    auto binWidth = hVec[i]->GetBinWidth(j);
+                    auto binVal = hVec[i]->GetBinContent(j);
+                    auto binError = hVec[i]->GetBinError(j);
+                    hVec[i]->SetBinContent(j, binVal/binWidth);
+                    hVec[i]->SetBinError(j, binError/binWidth);
+                }
+                hVec[i]->Scale(nEvents/hVec[i]->Integral());
+            
+                fitSPEMIP(hVec[i].get(), i);
+            }
+        }
+
     }
     catch(const NTRException& e)
     {
